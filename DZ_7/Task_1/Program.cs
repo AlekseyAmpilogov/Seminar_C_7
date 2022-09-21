ab3b6bbb38ffdf505419309dcfab28c6456a8884
Aleksey <pilot-1981@yandex.ru>
2022-09-21 17:05:17 +0500

    add DZ_7_Task_1.1

---
+++ DZ_7/Task_1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+double[,] array = new double[3, 4];
+Random rnd = new Random();
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = Convert.ToDouble(rnd.Next(-1000, 1000)/100.0);
+        Console.Write("\t" + array[i, j]);
+    }
+    Console.WriteLine();
+}

8b0d2b91dc97a578028ae858bc719b04ee9716eb
Aleksey <pilot-1981@yandex.ru>
2022-09-21 10:19:48 +0500

    add Sem_7_Task_2.2

--- Sem_7/Task_2/Program.cs
+++ Sem_7/Task_2/Program.cs
@@ -24,7 +24,7 @@ for (int i = 0; i < 4; i++)
 {
     for (int j = 0; j < 5; j++)
     {
-        if (i % 2 == 0 && j % 2 == 0 && i != 0 && j != 0)
+        if (i % 2 == 0 && j % 2 == 0)
         {
             array[i, j] = func(array[i, j]);
         }

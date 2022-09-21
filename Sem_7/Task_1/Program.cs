e2bfb1dc2bec461f71d225b4a9f16c01ff71eece
Aleksey <pilot-1981@yandex.ru>
2022-09-21 09:59:45 +0500

    add Sem_7_Task_2.1

--- Sem_7/Task_1/Program.cs
+++ Sem_7/Task_1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите количество строк в двумерном массиве: ");
+int strsCount = Convert.ToInt32(Console.ReadLine()); // число строк
+Console.Write("Введите количество столбцов в двумерном массиве: ");
+int columnsCount = Convert.ToInt32(Console.ReadLine()); // число столбцов
+
+int[,] twoDimArray = new int[strsCount, columnsCount]; //создаем массив
+
+Random rnd = new Random();
+for (int i = 0; i < strsCount; i++)
+{
+    for (int j=0; j<columnsCount;j++) // вложенный цикл for выводит очередную строку
+    {
+        twoDimArray[i, j] = rnd.Next(1, 100); // заполняем массив числами от 1 до 100
+        Console.Write("\t" + twoDimArray[i, j]); //выводим на экран полученный массив
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
---
+++ Sem_7/Task_2/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+
+int[,] array = new int[4, 5];
+Random rnd = new Random();
+
+Console.WriteLine("Изначальный массив: ");
+for (int i = 0; i < 4; i++)
+{
+    for (int j = 0; j < 5; j++)
+    {
+        array[i, j] = rnd.Next(1, 9);
+        Console.Write("\t" + array[i, j]);
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Преобразованный массив: ");
+for (int i = 0; i < 4; i++)
+{
+    for (int j = 0; j < 5; j++)
+    {
+        if (i % 2 == 0 && j % 2 == 0 && i !=0 && j !=0) 
+            array[i, j] = array[i, j] * array[i, j];
+        Console.Write("\t" + array[i, j]);
+    }
+    Console.WriteLine();
+}

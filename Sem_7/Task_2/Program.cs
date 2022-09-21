9d1a8ea4610a6bad450818c624ee76662ae7c073
Aleksey <pilot-1981@yandex.ru>
2022-09-21 10:16:51 +0500

    add Sem_7_Task_2.2

--- Sem_7/Task_2/Program.cs
+++ Sem_7/Task_2/Program.cs
@@ -3,6 +3,11 @@
 int[,] array = new int[4, 5];
 Random rnd = new Random();
 
+int func(int elem)
+{
+    return elem * elem;
+}
+
 Console.WriteLine("Изначальный массив: ");
 for (int i = 0; i < 4; i++)
 {
@@ -19,8 +24,10 @@ for (int i = 0; i < 4; i++)
 {
     for (int j = 0; j < 5; j++)
     {
-        if (i % 2 == 0 && j % 2 == 0 && i !=0 && j !=0) 
-            array[i, j] = array[i, j] * array[i, j];
+        if (i % 2 == 0 && j % 2 == 0 && i != 0 && j != 0)
+        {
+            array[i, j] = func(array[i, j]);
+        }
         Console.Write("\t" + array[i, j]);
     }
     Console.WriteLine();

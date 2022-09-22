66a2bd3179618519029f671e702121e302dd5b04
Aleksey <pilot-1981@yandex.ru>
2022-09-22 11:34:40 +0500

    add DZ_7_Task_3

---
+++ DZ_7/Task_3/Program.cs
@@ -0,0 +1,30 @@
+﻿double[,] array = new double[5, 5]; //задаем массиву количесто строк и столбцов
+Random rnd = new Random();
+newArray(array);
+avgArray(array);
+void newArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(rnd.Next(1, 10));
+            Console.Write(" {0} ", array[i, j]); //вывод полученного массива построчно
+        }
+        Console.WriteLine();
+    }
+}
+
+void avgArray(double[,] array)
+{
+    Console.Write("\nСреднеарифметическое каждого столбца = ");
+    for (int j = 0; j < array.GetLength(0); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < array.GetLength(1); i++)
+        {
+            sum = sum + array[i, j];
+        }
+        Console.Write($" {sum / array.GetLength(0)} ");
+    }
+}
